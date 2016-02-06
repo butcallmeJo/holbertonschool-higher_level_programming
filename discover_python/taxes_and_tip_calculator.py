aeb2047c6a876bde0484adf3564d22028973d713
Josquin Gaillard <13@p4-6.hbtn.io>
2016-02-06 10:23:17 -0800

    python discovery workshop done yay

---
+++ discover_python/taxes_and_tip_calculator.py
@@ -0,0 +1,10 @@
+# Python Workshop price calculator program.
+# Taking input from users about price,
+# taxes and tips and calculating the total.
+
+print "Welcome to the taxes and tip calculator!"
+price = float(input("What is the price before tax? "))
+tax = float(input("What are the taxes? (in %) "))
+tip = float(input("What do you want to tip (in %) "))
+total = price + (price * (tax/100)) + ((price + (price * (tax/100))) * (tip/100))
+print "The price you need to pay is: $%f " % (total)
