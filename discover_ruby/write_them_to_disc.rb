d0ff5c9bf25cd883bbf889f0ecdaac9584d83426
butcallmejo <13@holbertonschool.com>
2016-03-19 16:59:33 -0700

    finished 1

---
+++ discover_ruby/write_them_to_disc.rb
@@ -0,0 +1,13 @@
+require 'httpclient'
+
+extheaders = {
+  'User-Agent' => 'Holberton_School',
+  'Authorization' => 'token fffa38b10948aa7eff293682308672bc95672ae3'
+}
+
+http = HTTPClient.new
+response = http.get_content("https://api.github.com/search/repositories?q=language:ruby&sort=stars&order=desc", nil, extheaders)
+f = File.new("/tmp/13", "w")
+f.write(response)
+
+puts "The file was saved!"
