86a70e80111a6e28e0a774b7aa4f05e17856600f
butcallmejo <13@holbertonschool.com>
2016-03-19 17:36:25 -0700

    finished task 2

---
+++ discover_ruby/manipulate_the_data.rb
@@ -0,0 +1,16 @@
+require 'httpclient'
+require 'json'
+
+extheaders = {
+  'User-Agent' => 'Holberton_School',
+  'Authorization' => 'token fffa38b10948aa7eff293682308672bc95672ae3'
+}
+
+http = HTTPClient.new
+response = http.get_content("https://api.github.com/search/repositories?q=language:ruby&sort=stars&order=desc", nil, extheaders)
+
+json = JSON.parse(response)
+
+# puts json["items"][0]["full_name"]
+
+json["items"].map {|n| puts n["full_name"]}
