b3b7d311f38ee7db142b9c0d4cf31a2e020db517
butcallmejo <13@holbertonschool.com>
2016-04-01 17:12:51 -0700

    finished task 0 i think

--- discover_more_python/github_most_popular.py
+++ discover_more_python/github_most_popular.py
@@ -1,4 +1,11 @@
+import requests
+
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars&order=desc'
 request_headers = {
   'User-Agent': 'Holberton_School',
   'Authorization': 'token fffa38b10948aa7eff293682308672bc95672ae3'
 }
+
+r = requests.get(url, headers = request_headers)
+
+print r.content
