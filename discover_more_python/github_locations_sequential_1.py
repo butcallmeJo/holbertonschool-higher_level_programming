0f004dce94429544693a409a85c39a569080c3dc
butcallmejo <13@holbertonschool.com>
2016-04-03 12:07:20 -0700

    getting somewhere on seq 1

--- discover_more_python/github_locations_sequential_1.py
+++ discover_more_python/github_locations_sequential_1.py
@@ -1,4 +1,21 @@
+import requests, json
+
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars&order=desc'
 request_headers = {
   'User-Agent': 'Holberton_School',
   'Authorization': 'token fffa38b10948aa7eff293682308672bc95672ae3'
 }
+
+#creating a variable with the response of request
+r = requests.get(url = url, headers = request_headers)
+
+data = [{
+    'user': u['owner']['login'],
+    'name': u['full_name']
+} for u in r.json()['items']]
+
+for d in data:
+    loc_url = "https://api.github.com/users/" + d['user']
+    location_r = requests.get(url = loc_url, headers = request_headers)
+    d['location'] = location_r.json()['location']
+    #print json.dumps(d['location']) //not working exactly
