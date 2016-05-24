7a551344be5d9d8977ce28d63cc93e91e754ed1e
butcallmejo <josquin.gaillard@gmail.com>
2016-05-24 14:47:02 -0700

    finished task 2

--- manage_the_school_with_swift/person.swift
+++ manage_the_school_with_swift/person.swift
@@ -1,7 +1,7 @@
 // Script that describes a Person based on input given by a main func
 
+// super class for Person. init name and age
 class Person {
-  // super class for Person. init name and age
   var first_name: String
   var last_name: String
   var age: Int
@@ -23,16 +23,40 @@ protocol Classify {
   func isStudent() -> Bool
 }
 
+// enum to define the type of subject done by a mentor
+enum Subject {
+  case Math
+  case English
+  case French
+  case History
+}
+
+// sub-class of Person to describe mentors
 class Mentor: Person, Classify {
-  // sub-class of Person to describe mentors
+  let subject: Subject
+  // overloading constructor
+  init(first_name: String, last_name: String, age: Int, subject: Subject = Subject.Math) {
+    self.subject = subject
+    super.init(first_name: first_name, last_name: last_name, age: age)
+  }
+
+  // func that returns the subject as a String
+  func stringSubject() -> String {
+    switch subject {
+      case .Math: return "Math"
+      case .English: return "English"
+      case .French: return "French"
+      case .History: return "History"
+    }
+  }
   // func that returns false because Person is not student
   func isStudent() -> Bool {
     return false
   }
 }
 
+// sub-class of Person to describe students
 class Student: Person, Classify {
-  // sub-class of Person to describe students
   // func that returns true because Person is student
   func isStudent() -> Bool {
     return true
