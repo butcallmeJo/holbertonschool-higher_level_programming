6abdc854627e3d6a9e3fd5147671601bf766abcb
butcallmejo <josquin.gaillard@gmail.com>
2016-05-25 10:15:27 -0700

    renamed and changed a couple stuff on level 0

--- hodor/level_0/hodo_script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-for (( i = 0; i < 1024; i++ )); do
-  curl 'http://173.246.108.142/level0.php' -H 'Origin: http://173.246.108.142' -H 'Accept-Encoding: gzip, deflate' -H 'Accept-Language: en-US,en;q=0.8' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/50.0.2661.102 Chrome/50.0.2661.102 Safari/537.36' -H 'Content-Type: application/x-www-form-urlencoded' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Cache-Control: max-age=0' -H 'Referer: http://173.246.108.142/level0.php' -H 'Connection: keep-alive' --data 'id=1337&holdthedoor=Submit' --compressed
-done
