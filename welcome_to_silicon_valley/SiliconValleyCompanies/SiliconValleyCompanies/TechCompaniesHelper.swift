f0484aece9d9da768ed612c7f9b63766ee8a01e4
Josquin Gaillard <13@p4-3.hbtn.io>
2016-05-27 13:13:25 -0700

    some commit for first ios app and some commit for silicon valley app

---
+++ welcome_to_silicon_valley/SiliconValleyCompanies/SiliconValleyCompanies/TechCompaniesHelper.swift
@@ -0,0 +1,17 @@
+//
+//  TechCompaniesHelper.swift
+//  SiliconValleyCompanies
+//
+//  Created by Josquin Gaillard on 5/27/16.
+//  Copyright © 2016 Josquin Gaillard. All rights reserved.
+//
+
+import Foundation
+
+class TechCompaniesHelper {
+    var companies:[String] = ["Holberton", "Linkedin", "Docker", "Google", "Yahoo", "Apple"]
+    
+    func getTechCompanies() -> [String] {
+        return companies
+    }
+}
