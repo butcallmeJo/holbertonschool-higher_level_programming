7abad3dc6c7f76e0366457fe44172882c5dc1b65
butcallmejo <josquin.gaillard@gmail.com>
2016-05-27 17:43:07 -0700

    finished task 2

--- mvc_let_s_interact_with_print/task_controller.py
+++ mvc_let_s_interact_with_print/task_controller.py
@@ -1,8 +1,18 @@
-
+import Tkinter as tk
+from task_model import TaskModel
+from task_view import TaskView
 
 class TaskController():
     """docstring for TaskController"""
     def __init__(self, master, model):
+        # Basic init stuff
         if not master or not isinstance(master, tk.Tk):
             raise Exception("master is not a tk.Tk()")
-        self.master = master
+        if not model or not isinstance(model, TaskModel):
+            raise Exception("model is not a TaskModel")
+        self.__model = model
+        self.__view = TaskView(master)
+        # connecting all the dots.
+        self.__view.update_title(self.__model.get_title())
+        self.__model.set_callback_title(self.__view.update_title)
+        self.__view.toggle_button.config(command=self.__model.toggle)
