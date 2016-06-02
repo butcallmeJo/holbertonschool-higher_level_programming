cff49750e54e1c06ed8e235513f0e7045fc82991
butcallmejo <josquin.gaillard@gmail.com>
2016-06-02 16:02:14 -0700

    finished task 2

---
+++ basic_sql_playground/2-main.sql
@@ -0,0 +1,3 @@
+insert into Person (first_name, last_name, age) values ("Jon", "Snow", 26);
+insert into Person (first_name, last_name, age) values ("Arya", "Stark", 12);
+select * from Person order by last_name;
